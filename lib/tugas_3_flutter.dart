2739241feef85aabedcab28cf3e1c3494a0f7637
hernando_ferry <hernando.ferry@gmail.com>
2026-05-13 08:55:00 +0700

    fix : Tugas 3 flutter form input dan grid layouting

--- lib/tugas_3_flutter.dart
+++ lib/tugas_3_flutter.dart
@@ -5,96 +5,123 @@ class Tugas3Flutter extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final List<Map<String, String>> dataListGambar = [
+      {'nama': 'Dodol Betawi', 'gambar': 'assets/images/dodol_betawi.jpg'},
+      {
+        'nama': 'Kerak Telor',
+        'gambar':
+            'assets/images/kerak_telor.jpg', // Ganti sesuai nama file Anda
+      },
+      {
+        'nama': 'Soto Betawi',
+        'gambar':
+            'assets/images/soto_betawi.jpeg', // Ganti sesuai nama file Anda
+      },
+      {
+        'nama': 'Semur Jengkol',
+        'gambar':
+            'assets/images/semur_jengkol.jpeg', // Ganti sesuai nama file Anda
+      },
+
+      {
+        'nama': 'Selendang Mayang',
+        'gambar':
+            'assets/images/selendang_mayang.png', // Ganti sesuai nama file Anda
+      },
+      {
+        'nama': 'Kue Rangi',
+        'gambar': 'assets/images/kue_rangi.jpg', // Ganti sesuai nama file Anda
+      },
+    ];
+
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Colors.amberAccent,
-        title: Text("Tugas Flutter 3 (form input dan grid)"),
+        title: const Text("Tugas Flutter 3 (form input dan grid)"),
       ),
-      body: Column(
-        children: [
-          SingleChildScrollView(
-            child: Column(
-              children: [
-                SizedBox(height: 11),
-                TextField(
-                  decoration: InputDecoration(
-                    hintText: "Ex: Jhon Maer",
-                    labelText: "Nama Lengkap :",
-                    border: OutlineInputBorder(),
-                  ),
-                ),
-                SizedBox(height: 11),
-                TextField(
-                  decoration: InputDecoration(
-                    labelText: "No. Hp/Wa/Telp",
-                    hintText: "Ex : 081123456789 / 0219876543",
-                    border: OutlineInputBorder(),
-                  ),
-                ),
-                SizedBox(height: 11),
-                TextField(
-                  decoration: InputDecoration(
-                    labelText: "E-mail",
-                    hintText: "Ex : contoh@email.co.id",
-                    border: OutlineInputBorder(),
-                  ),
-                ),
-                SizedBox(height: 11),
-                TextField(
-                  decoration: InputDecoration(
-                    labelText: "Alamat",
-                    hintText: "Ex : Jl.Karet No.1 Jakarta",
-                    border: OutlineInputBorder(),
-                  ),
-                ),
-              ],
+      body: SingleChildScrollView(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            const TextField(
+              decoration: InputDecoration(
+                hintText: "Ex: Jhon Maer",
+                labelText: "Nama Lengkap :",
+                border: OutlineInputBorder(),
+              ),
             ),
-          ),
-          SizedBox(height: 11),
-          GridView.builder(
-            shrinkWrap: true,
-            physics: const NeverScrollableScrollPhysics(),
-            gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-              crossAxisCount: 2,
-              crossAxisSpacing: 10,
-              mainAxisSpacing: 10,
+            const SizedBox(height: 11),
+            const TextField(
+              decoration: InputDecoration(
+                labelText: "No. Hp/Wa/Telp :",
+                hintText: "Ex : 081123456789 / 0219876543",
+                border: OutlineInputBorder(),
+              ),
             ),
-            itemCount: 4,
-            itemBuilder: (context, index) {
-              return ClipRRect(
-                borderRadius: BorderRadius.circular(15),
-                child: Stack(
-                  alignment: Alignment.bottomCenter,
-                  children: [
-                    Container(
-                      width: double.infinity,
-                      height: double.infinity,
-                      color: Colors.amber,
-                      child: Image.asset(
-                        'assets/images/dodol_betawi.jpg',
-                        fit: BoxFit.cover,
-                      ),
-                    ),
-                    Container(
-                      width: double.infinity,
-                      height: 45,
-                      padding: EdgeInsets.all(11),
-                      color: Colors.amber,
-                      child: Text(
-                        'Dodol Betawi',
-                        style: TextStyle(
-                          fontWeight: FontWeight.bold,
-                          fontSize: 15,
-                          color: Colors.black,
+            const SizedBox(height: 11),
+            const TextField(
+              decoration: InputDecoration(
+                labelText: "E-mail :",
+                hintText: "Ex : contoh@email.co.id",
+                border: OutlineInputBorder(),
+              ),
+            ),
+            const SizedBox(height: 11),
+            const TextField(
+              decoration: InputDecoration(
+                labelText: "Alamat :",
+                hintText: "Ex : Jl.Karet No.1 Jakarta",
+                border: OutlineInputBorder(),
+              ),
+            ),
+            const SizedBox(height: 20),
+            GridView.builder(
+              shrinkWrap: true,
+              physics: const ScrollPhysics(),
+              gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                crossAxisCount: 2,
+                crossAxisSpacing: 10,
+                mainAxisSpacing: 10,
+              ),
+              itemCount: dataListGambar.length,
+              itemBuilder: (context, index) {
+                final gambar = dataListGambar[index];
+                return ClipRRect(
+                  borderRadius: BorderRadius.circular(15),
+                  child: Stack(
+                    alignment: Alignment.bottomCenter,
+                    children: [
+                      Container(
+                        width: double.infinity,
+                        height: double.infinity,
+                        color: Colors.amber,
+                        child: Image.asset(
+                          gambar['gambar'] ?? 'no-image',
+                          fit: BoxFit.cover,
                         ),
                       ),
-                    ),
-                  ],
-                ),
-              );
-            },
-          ),
-        ],
+                      Container(
+                        width: double.infinity,
+                        height: 45,
+                        padding: const EdgeInsets.all(11),
+                        color: Colors.amber,
+                        child: Text(
+                          gambar['nama'] ?? 'no-name-produk',
+                          style: TextStyle(
+                            fontWeight: FontWeight.bold,
+                            fontSize: 15,
+                            color: Colors.black,
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                );
+              },
+            ),
+          ],
+        ),
       ),
     );
   }
