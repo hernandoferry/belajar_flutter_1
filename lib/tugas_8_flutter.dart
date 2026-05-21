9fd3164c6ebf0e0b1bf805b8fe392b3788472c12
hernando_ferry <hernando.ferry@gmail.com>
2026-05-21 20:55:09 +0700

    fix: Tugas 8 flutter

--- lib/tugas_8_flutter.dart
+++ lib/tugas_8_flutter.dart
@@ -9,6 +9,7 @@ class Tugas8Flutter extends StatefulWidget {
 
 class _Tugas8FlutterState extends State<Tugas8Flutter> {
   int _selectedIndex = 0;
+  final List<String> judulAppBar = ['Home', 'Tentang Aplikasi'];
 
   static const List<Widget> _widgetOptions = <Widget>[
     Text("index 0 : Home"),
@@ -24,9 +25,27 @@ class _Tugas8FlutterState extends State<Tugas8Flutter> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.amber,
+        title: RichText(
+          text: TextSpan(
+            // Gabungkan teks statis dan variabel di dalam satu pasang tanda petik ""
+            text: "Tugas 8 flutter : ${judulAppBar[_selectedIndex]}",
+            style: const TextStyle(
+              color: Colors.black,
+              fontSize: 16,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+        ),
+      ),
+
       body: Center(child: _widgetOptions.elementAt(_selectedIndex)),
       bottomNavigationBar: BottomNavigationBar(
         currentIndex: _selectedIndex,
+        onTap: _onItemTapped,
+        selectedItemColor: Colors.amberAccent,
+        unselectedItemColor: Colors.grey,
         items: const <BottomNavigationBarItem>[
           BottomNavigationBarItem(icon: Icon(Icons.home), label: 'Home'),
           BottomNavigationBarItem(
